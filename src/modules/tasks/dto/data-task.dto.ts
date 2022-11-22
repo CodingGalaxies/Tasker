a6dda67cb2945310800833ebfd28bc55bf6f4329
andres06-hub <andressierrarojas7@gmail.com>
2022-11-21 21:53:10 -0500

    CREATED: module of tasks

---
+++ src/modules/tasks/dto/data-task.dto.ts
@@ -0,0 +1,7 @@
+export class DataTask {
+  title: string;
+  description: string;
+  points: string;
+  assigned?: string;
+  status: string;
+}
---
+++ src/modules/tasks/tasks.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TasksService {
+  createTask() {
+    //
+  }
+
+  updateTask(id: string) {
+    //
+  }
+
+  findTaks(id: string) {
+    //
+  }
+}

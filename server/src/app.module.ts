6e48bb42ed946eccfbcc583605bf3b5e3b931138
andres06-hub <andressierrarojas7@gmail.com>
2022-11-22 00:07:05 -0500

    UPDATE: #2 requested changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,9 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/tasker_db', {
-      //
-    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/tasker_db'),
     TasksModule,
     UserModule,
   ],
--- server/src/modules/tasks/tasks.service.ts
+++ server/src/modules/tasks/tasks.service.ts
@@ -3,15 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TasksService {
   createTask() {
-    //
+    return;
   }
 
   updateTask(id: string) {
-    //
+    return;
   }
 
   async findTaks(id: string): Promise<boolean> {
-    //
     return true;
   }
 }

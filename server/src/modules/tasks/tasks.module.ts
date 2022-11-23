ad478fc5298bcdc9d0b0f523e9d45434823abf49
andres06-hub <andressierrarojas7@gmail.com>
2022-11-22 22:31:44 -0500

    CREATED: End-Point for task and services of connection to DB

--- server/src/modules/tasks/tasks.module.ts
+++ server/src/modules/tasks/tasks.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskSchema } from 'src/models/tasks/schemas/task.schema';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }])],
   controllers: [TasksController],
   providers: [TasksService],
+  exports: [TasksService],
 })
 export class TasksModule {}
--- server/src/modules/tasks/tasks.service.ts
+++ server/src/modules/tasks/tasks.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task, TaskDocument } from '../../models/tasks/schemas/task.schema';
+import { DataTask } from '../tasks/dto/data-task.dto';
 
 @Injectable()
 export class TasksService {
-  createTask() {
-    return;
+  constructor(@InjectModel('Task') private taskModel: Model<TaskDocument>) {}
+
+  async createTask(data: DataTask): Promise<boolean> {
+    console.table(data);
+    const newTask: Task = await new this.taskModel(data).save();
+    console.log(`newTask: ${newTask}`);
+    return true;
   }
 
   updateTask(id: string) {
-    return;
+    return id;
   }
 
-  async findTaks(id: string): Promise<boolean> {
-    return true;
+  async findOneTask(id: string): Promise<Task> {
+    return await this.taskModel.findOne({ id }).exec();
   }
 }

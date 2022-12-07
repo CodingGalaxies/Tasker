ba4b2f5105740b8f1a83f15b07bd19796f54b3bb
andres06-hub <andressierrarojas7@gmail.com>
2022-12-06 21:06:02 -0500

    MODIFIED-ADD: config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { UserModule } from './modules/user/user.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from './config/config.module';
 import { AppConfigService } from './config/app/app.config.service';
 import { key } from './config/config.key';
+import { ProvidersModule } from './providers/providers.module';
+import { ModulesModule } from './modules/modules.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
-    ConfigModule,
     AppModule,
-    MongooseModule.forRoot(`mongodb://localhost:27017/tasker_db`),
-    TasksModule,
+    ConfigModule,
+    ProvidersModule,
+    // TasksModule,
     UserModule,
+    ModulesModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
